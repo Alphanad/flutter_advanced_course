9b1fdaf26e9cc52fac7fe246d634748d99c7cbca
Alphanad <Alphanad.Official@gmail.com>
2025-09-07 13:41:14 +0330

    step 3: add repositories and data state

---
+++ lib/features/feature_weather/data/repository/weather_repositoryimpl.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_advanced_course/core/resorces/data_state.dart';
+import 'package:flutter_advanced_course/features/feature_weather/data/models/current_city_model.dart';
+import 'package:flutter_advanced_course/features/feature_weather/domain/entities/current_city_entity.dart';
+import 'package:flutter_advanced_course/features/feature_weather/data/data_source/remote/api_provider.dart';
+import 'package:flutter_advanced_course/features/feature_weather/domain/repository/weather_repository.dart';
+
+class WeatherRepositoryImpl extends WeatherRepository {
+  ApiProvider apiProvider;
+
+  WeatherRepositoryImpl(this.apiProvider);
+
+  @override
+  Future<DataState<CurrentCityEntity>> fetchCurrentWeatherData(
+    String cityName,
+  ) async {
+    try {
+      Response response = await apiProvider.callCurrentWeather(cityName);
+      if (response.statusCode == 200) {
+        CurrentCityEntity currentCityEntity = CurrentCityModel.fromJson(
+          response.data,
+        );
+        return DataSuccess(currentCityEntity);
+      } else {
+        return DataFailed(
+          'Something went wrong, try again... ${response.statusCode}',
+        );
+      }
+    } catch (e) {
+      return DataFailed('Please check your connection...');
+    }
+  }
+}
