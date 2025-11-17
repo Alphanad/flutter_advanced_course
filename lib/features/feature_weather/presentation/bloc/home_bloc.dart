add86e39ebf8192c83917f7f1cac65b2266c60a3
Alphanad <Alphanad.Official@gmail.com>
2025-11-17 16:05:49 +0330

    add bloc and event and state

---
+++ lib/features/feature_weather/presentation/bloc/home_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+
+  HomeBloc() : super(HomeInitial()) {
+
+    on<LoadCwEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
